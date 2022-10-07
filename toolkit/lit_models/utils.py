507e42dfa0c4d50c632223288c45b9ed0649c302
cheasim <852975133@qq.com>
2022-10-07 16:59:13 +0800

    update ema callbacks

--- toolkit/lit_models/utils.py
+++ toolkit/lit_models/utils.py
@@ -169,103 +169,135 @@ class SparseMax_good(nn.Module):
 from copy import deepcopy
 from typing import Optional, Union, Dict, Any
 
-import pytorch_lightning as pl
 import torch
 from pytorch_lightning.utilities import rank_zero_only
 
 
-class EMA(pl.Callback):
-    """Implements EMA (exponential moving average) to any kind of model.
-    EMA weights will be used during validation and stored separately from original model weights.
+import logging
 
-    How to use EMA:
-        - Sometimes, last EMA checkpoint isn't the best as EMA weights metrics can show long oscillations in time. See
-          https://github.com/rwightman/pytorch-image-models/issues/102
-        - Batch Norm layers and likely any other type of norm layers doesn't need to be updated at the end. See
-          discussions in: https://github.com/rwightman/pytorch-image-models/issues/106#issuecomment-609461088 and
-          https://github.com/rwightman/pytorch-image-models/issues/224
-        - For object detection, SWA usually works better. See   https://github.com/timgaripov/swa/issues/16
+import pytorch_lightning as pl
+import torch
+from pytorch_lightning.callbacks import Callback
+from pytorch_lightning.utilities import rank_zero_only
 
-    Implementation detail:
-        - See EMA in Pytorch Lightning: https://github.com/PyTorchLightning/pytorch-lightning/issues/10914
-        - When multi gpu, we broadcast ema weights and the original weights in order to only hold 1 copy in memory.
-          This is specially relevant when storing EMA weights on CPU + pinned memory as pinned memory is a limited
-          resource. In addition, we want to avoid duplicated operations in ranks != 0 to reduce jitter and improve
-          performance.
+def unwrap_model(model):
+    if isinstance(model, ModelEmaV2):
+        return unwrap_model(model.ema)
+    else:
+        return model.module if hasattr(model, 'module') else model
+
+
+def get_state_dict(model, unwrap_fn=unwrap_model):
+    return unwrap_fn(model).state_dict()
+
+
+class ModelEmaV2(nn.Module):
+    """ Model Exponential Moving Average V2
+    Keep a moving average of everything in the model state_dict (parameters and buffers).
+    V2 of this module is simpler, it does not match params/buffers based on name but simply
+    iterates in order. It works with torchscript (JIT of full model).
+    This is intended to allow functionality like
+    https://www.tensorflow.org/api_docs/python/tf/train/ExponentialMovingAverage
+    A smoothed version of the weights is necessary for some training schemes to perform well.
+    E.g. Google's hyper-params for training MNASNet, MobileNet-V3, EfficientNet, etc that use
+    RMSprop with a short 2.4-3 epoch decay period and slow LR decay rate of .96-.99 requires EMA
+    smoothing of weights to match results. Pay attention to the decay constant you are using
+    relative to your update count per epoch.
+    To keep EMA from using GPU resources, set device='cpu'. This will save a bit of memory but
+    disable validation of the EMA weights. Validation will have to be done manually in a separate
+    process, or after the training stops converging.
+    This class is sensitive where it is initialized in the sequence of model init,
+    GPU assignment and distributed training wrappers.
     """
-    def __init__(self, decay = 0.9999,
-        ema_device = None,
-        pin_memory=True):
-        super().__init__()
+    def __init__(self, model, decay=0.9999, device=None):
+        super(ModelEmaV2, self).__init__()
+        # make a copy of the model for accumulating moving average of weights
+        self.module = deepcopy(model)
+        self.module.eval()
         self.decay = decay
-        self.ema_device: str = f"{ema_device}" if ema_device else None  # perform ema on different device from the model
-        self.ema_pin_memory = pin_memory if torch.cuda.is_available() else False  # Only works if CUDA is available
-        self.ema_state_dict: Dict[str, torch.Tensor] = {}
-        self.original_state_dict = {}
-        self._ema_state_dict_ready = False
+        self.device = device  # perform ema on different device from model if set
+        if self.device is not None:
+            self.module.to(device=device)
 
-    @staticmethod
-    def get_state_dict(pl_module: pl.LightningModule):
-        """Returns state dictionary from pl_module. Override if you want filter some parameters and/or buffers out.
-        For example, in pl_module has metrics, you don't want to return their parameters.
-        
-        code:
-            # Only consider modules that can be seen by optimizers. Lightning modules can have others nn.Module attached
-            # like losses, metrics, etc.
-            patterns_to_ignore = ("metrics1", "metrics2")
-            return dict(filter(lambda i: i[0].startswith(patterns), pl_module.state_dict().items()))
-        """
-        return pl_module.state_dict()
-        
-    def on_train_start(self, trainer: "pl.Trainer", pl_module: pl.LightningModule) -> None:
-        # Only keep track of EMA weights in rank zero.
-        if not self._ema_state_dict_ready and pl_module.global_rank == 0:
-            self.ema_state_dict = deepcopy(self.get_state_dict(pl_module))
-            if self.ema_device:
-                self.ema_state_dict = {k: tensor.to(device=self.ema_device) for k, tensor in self.ema_state_dict.items()}
-
-            if self.ema_device == "cpu" and self.ema_pin_memory:
-                self.ema_state_dict = {k: tensor.pin_memory() for k, tensor in self.ema_state_dict.items()}
-
-        self._ema_state_dict_ready = True
-
-    @rank_zero_only
-    def on_train_batch_end(self, trainer: "pl.Trainer", pl_module: pl.LightningModule, *args, **kwargs) -> None:
-        # Update EMA weights
+    def _update(self, model, update_fn):
         with torch.no_grad():
-            for key, value in self.get_state_dict(pl_module).items():
-                ema_value = self.ema_state_dict[key]
-                ema_value.copy_(self.decay * ema_value + (1. - self.decay) * value, non_blocking=True)
+            for ema_v, model_v in zip(self.module.state_dict().values(), model.state_dict().values()):
+                if self.device is not None:
+                    model_v = model_v.to(device=self.device)
+                ema_v.copy_(update_fn(ema_v, model_v))
 
-    def on_validation_start(self, trainer: pl.Trainer, pl_module: pl.LightningModule) -> None:
-        if not self._ema_state_dict_ready:
-            return  # Skip Lightning sanity validation check if no ema weights has been loaded from a checkpoint.
+    def update(self, model):
+        self._update(model, update_fn=lambda e, m: self.decay * e + (1. - self.decay) * m)
 
-        self.original_state_dict = deepcopy(self.get_state_dict(pl_module))
-        pl_module.trainer.training_type_plugin.broadcast(self.ema_state_dict, 0)
-        assert self.ema_state_dict.keys() == self.original_state_dict.keys(), \
-            f"There are some keys missing in the ema static dictionary broadcasted. " \
-            f"They are: {self.original_state_dict.keys() - self.ema_state_dict.keys()}"
-        pl_module.load_state_dict(self.ema_state_dict, strict=False)
+    def set(self, model):
+        self._update(model, update_fn=lambda e, m: m)
 
-        if pl_module.global_rank > 0:
-            # Remove ema state dict from the memory. In rank 0, it could be in ram pinned memory.
-            self.ema_state_dict = {}
+class EMACallback(Callback):
+    """
+    Model Exponential Moving Average. Empirically it has been found that using the moving average
+    of the trained parameters of a deep network is better than using its trained parameters directly.
+    If `use_ema_weights`, then the ema parameters of the network is set after training end.
+    """
 
-    def on_validation_end(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
-        if not self._ema_state_dict_ready:
-            return  # Skip Lightning sanity validation check if no ema weights has been loaded from a checkpoint.
+    def __init__(self, decay=0.9999, use_ema_weights: bool = True):
+        self.decay = decay
+        self.ema = None
+        self.use_ema_weights = use_ema_weights
 
-        # Replace EMA weights with training weights
-        pl_module.load_state_dict(self.original_state_dict, strict=False)
+    def on_fit_start(self, trainer, pl_module):
+        "Initialize `ModelEmaV2` from timm to keep a copy of the moving average of the weights"
+        self.ema = ModelEmaV2(pl_module, decay=self.decay, device=None)
 
-    def on_save_checkpoint(
-        self, trainer: "pl.Trainer", pl_module: "pl.LightningModule", checkpoint: Dict[str, Any]
-    ) -> dict:
-        return {"ema_state_dict": self.ema_state_dict, "_ema_state_dict_ready": self._ema_state_dict_ready}
+    def on_train_batch_end(
+        self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx
+    ):
+        "Update the stored parameters using a moving average"
+        # Update currently maintained parameters.
+        self.ema.update(pl_module)
 
-    def on_load_checkpoint(
-        self, trainer: "pl.Trainer", pl_module: "pl.LightningModule", callback_state: Dict[str, Any]
-    ) -> None:
-        self._ema_state_dict_ready = callback_state["_ema_state_dict_ready"]
-        self.ema_state_dict = callback_state["ema_state_dict"]
+    def on_validation_epoch_start(self, trainer, pl_module):
+        "do validation using the stored parameters"
+        # save original parameters before replacing with EMA version
+        self.store(pl_module.parameters())
+
+        # update the LightningModule with the EMA weights
+        # ~ Copy EMA parameters to LightningModule
+        self.copy_to(self.ema.module.parameters(), pl_module.parameters())
+
+    def on_validation_end(self, trainer, pl_module):
+        "Restore original parameters to resume training later"
+        self.restore(pl_module.parameters())
+
+    def on_train_end(self, trainer, pl_module):
+        # update the LightningModule with the EMA weights
+        if self.use_ema_weights:
+            self.copy_to(self.ema.module.parameters(), pl_module.parameters())
+            msg = "Model weights replaced with the EMA version."
+            # log_main_process(_logger, logging.INFO, msg)
+
+    def on_save_checkpoint(self, trainer, pl_module, checkpoint):
+        if self.ema is not None:
+            return {"state_dict_ema": get_state_dict(self.ema, unwrap_model)}
+
+    def on_load_checkpoint(self, callback_state):
+        if self.ema is not None:
+            self.ema.module.load_state_dict(callback_state["state_dict_ema"])
+
+    def store(self, parameters):
+        "Save the current parameters for restoring later."
+        self.collected_params = [param.clone() for param in parameters]
+
+    def restore(self, parameters):
+        """
+        Restore the parameters stored with the `store` method.
+        Useful to validate the model with EMA parameters without affecting the
+        original optimization process.
+        """
+        for c_param, param in zip(self.collected_params, parameters):
+            param.data.copy_(c_param.data)
+
+    def copy_to(self, shadow_parameters, parameters):
+        "Copy current parameters into given collection of parameters."
+        for s_param, param in zip(shadow_parameters, parameters):
+            if param.requires_grad:
+                param.data.copy_(s_param.data)
