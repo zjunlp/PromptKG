b2bc7f58e9fe979aadbdc5f17debc610e5480a36
Wangxh-07 <61005954+Wangxh-07@users.noreply.github.com>
2023-03-22 20:01:47 +0800

    Create create_midres.py

---
+++ lambdaKG/LLM/create_midres.py
@@ -0,0 +1,42 @@
+#if you need to generate the MidRes.json by yourself, the following code may help you instruct smoothly.
+import json
+entity2textlong ={}
+corpus=[]
+middle_reason={}
+entity2text={}
+
+
+fr1=open("../dataset/FB15k-237/entity2text.txt",encoding='utf-8')
+for line in fr1.readlines():
+    lines=line.strip('\n').split('\t')
+    entity2text[lines[0]]=lines[1]
+
+fr2=open("../dataset/FB15k-237/entity2textlong.txt",encoding='utf-8')
+for line in fr2.readlines():
+    lines=line.strip('\n').split('\t')
+    entity2textlong[lines[0]]=lines[1]
+
+f=open("../dataset/FB15k-237/train.tsv")
+
+def build_midres():
+    for line in f.readlines():
+        lines= line.strip('\n').split('\t')
+        rel = lines[1].split('/')[-1].replace('_',' ')
+        corpus.append("what is the {} of {}? The answer is {}.\n".format(rel,entity2text[lines[0]],entity2text[lines[2]]))
+        find_relv=False
+        relv_text=''
+        for i in entity2textlong[lines[0]].replace('\\n',' ').split('. '):
+            if relv_text=='' and entity2text[lines[0]].lower() in i.lower():
+                relv_text+=i+'. '
+            if (entity2text[lines[2]]).lower() in i.lower() :
+                find_relv=True
+                relv_text+=' '+i+'.'
+        if find_relv is False:
+            relv_text=' '+entity2textlong[lines[0]].split('. ')[0]+'.'
+        middle_reason["what is the {} of {}? The answer is {}.\n".format(rel,entity2text[lines[0]],entity2text[lines[2]])]=relv_text
+
+wt=open("../dataset/FB15k-237/MidRes1.json",'w')
+build_midres()
+jstr=json.dumps(middle_reason)
+json.dump(jstr,wt)
+
